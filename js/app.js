b201d90d916e54c7e2f7c676c1f0babdb0b79d16
genaharmse-3 <harmsegena3@gmail.com>
2026-07-15 17:46:57 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    document.getElementById("main-title").textContent =
+        "One Tiny Heartbeat";
+
+    document.getElementById("subtitle").textContent =
+        "Every beautiful story begins with a single heartbeat.";
+
+    document.getElementById("subtitle").style.opacity = "1";
+
+    const button = document.getElementById("begin-button");
+
+    button.style.opacity = "1";
+    button.style.transform = "translateY(0)";
+
+});
